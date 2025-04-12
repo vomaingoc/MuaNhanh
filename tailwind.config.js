b9dc6be5a73d23b92352d7f64b18c724bb727133
Mai <13151788+vomaingoc@users.noreply.github.com>
2025-04-12 07:23:13 +0700

    UPDATE

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,12 @@ export default {
           900: "var(--primary-900)",
         },
         stone: {
-          50: "var(--stone-50)",
-          100: "var(--stone-100)",
-          200: "var(--stone-200)",
+          50: "#F8F7F5",
+          100: "#F2F2F0",
+          200: "#F0EFED",
         },
         zinc: {
-          800: "var(--zinc-800)",
+          800: "#282828",
         },
       },
     },
